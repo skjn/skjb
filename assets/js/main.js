7defa815cf6eda07cf2f62872a7243722706df3d
Shashank Jain <shashank.jain97@gmail.com>
2018-09-21 16:39:08 -0500

    Commit 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 
-(function($) {
+(function($){
 
 	skel.breakpoints({
 		xlarge:	'(max-width: 1680px)',
